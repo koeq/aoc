62f27d32b9dfbce89a82c36a5eef9ffb44baa136
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-04-10 14:41:20 +0200

    add mapping from string arr to num arr

--- ts/1.ts
+++ ts/1.ts
@@ -1,15 +1,35 @@
 import { getInput, liner } from "../file-reader.js";
 
-const input: string[] = getInput(liner);
+let input: string[] = getInput(liner);
 
-const getIncreases = (input: string[]): number => {
+// UTILS
+const toNum = (input: string[]): number[] => {
+  const mapped = input.map((element) => parseInt(element));
+
+  return mapped;
+};
+
+const inputs = toNum(input);
+
+// 1.1
+const getIncreases = (inputs: number[]): number => {
   let increases = 0;
-  for (let i = 0; i < input.length; i++) {
-    
-    parseInt(input[i]) < parseInt(input[i + 1]) ? increases++ : null;
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i] < inputs[i + 1] ? increases++ : null;
   }
 
   return increases;
 };
 
-console.log(getIncreases(input));
+// console.log(getIncreases(inputs));
+
+// 1.2
+const getIncreasedTriplets = (inputs: string[]): number => {
+  let increases = 0;
+
+  for (let i = 0; i < inputs.length; i++) {
+    parseInt(inputs[i]) < parseInt(inputs[i + 1]) ? increases++ : null;
+  }
+
+  return increases;
+};
