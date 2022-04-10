1abfb8f35dc97ac00e78227814835849ea8b94cb
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-04-10 14:23:23 +0200

    1-1

--- dist/1.js
+++ dist/1.js
@@ -1,4 +1,11 @@
 import { getInput, liner } from "../file-reader.js";
 const input = getInput(liner);
-console.log(input);
+const getIncreases = (input) => {
+    let increases = 0;
+    for (let i = 0; i < input.length; i++) {
+        parseInt(input[i]) < parseInt(input[i + 1]) ? increases++ : null;
+    }
+    return increases;
+};
+console.log(getIncreases(input));
 //# sourceMappingURL=1.js.map
--- ts/1.ts
+++ ts/1.ts
@@ -2,4 +2,14 @@ import { getInput, liner } from "../file-reader.js";
 
 const input: string[] = getInput(liner);
 
-console.log(input);
+const getIncreases = (input: string[]): number => {
+  let increases = 0;
+  for (let i = 0; i < input.length; i++) {
+    
+    parseInt(input[i]) < parseInt(input[i + 1]) ? increases++ : null;
+  }
+
+  return increases;
+};
+
+console.log(getIncreases(input));
