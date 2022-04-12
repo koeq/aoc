557ace2834ea1144fe11f380ee536a1142a8e1d1
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-04-12 14:15:34 +0200

    refactor: use ternary statement

--- src/2.ts
+++ src/2.ts
@@ -7,21 +7,36 @@ const calculatePosition = (input: string[]) => {
   let x = 0;
   let y = 0;
 
-  for (const direction of input) {
-    const splittedDirection = direction.split(" ");
+  for (const directionAmount of input) {
+    const splittedDirection = directionAmount.split(" ");
     const amount = parseInt(splittedDirection[1]);
+    const direction = splittedDirection[0];
 
-    switch (splittedDirection[0]) {
-      case "up":
-        y -= amount;
-        break;
-      case "down":
-        y += amount;
-        break;
-      case "forward":
-        x += amount;
-        break;
-    }
+    // switch (splittedDirection[0]) {
+    //   case "up":
+    //     y -= amount;
+    //     break;
+    //   case "down":
+    //     y += amount;
+    //     break;
+    //   case "forward":
+    //     x += amount;
+    //     break;
+    // }
+
+    // if (direction === "forward") {
+    //   x += amount;
+    // } else if (direction === "up") {
+    //   y -= amount;
+    // } else {
+    //   y += amount;
+    // }
+
+    direction === "forward"
+      ? (x += amount)
+      : direction === "up"
+      ? (y -= amount)
+      : (y += amount);
   }
 
   return x * y;
