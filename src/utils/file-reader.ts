d8dc0d8268e258305af7968ed3d46c70d9796df6
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-04-18 21:23:11 +0200

    4.1 handle input

--- src/utils/file-reader.ts
+++ src/utils/file-reader.ts
@@ -10,3 +10,49 @@ export const getInput = (fileNum: number) => {
   }
   return input;
 };
+
+interface BingoInput {
+  boards: (number | string | undefined)[][][];
+  nums: number[];
+}
+
+export const getBingoInput = () => {
+  const liner = new lineByLine(`./inputs/input4.txt`);
+  let boardsInput: number[][] = [];
+  let nums: number[] = [];
+  let lineCount = 1;
+  let boards = [];
+
+  let line;
+  while ((line = liner.next())) {
+    let decoded = line.toString("utf-8");
+
+    if (lineCount === 1) {
+      nums = decoded.split(",").map((entry) => parseInt(entry));
+    } else if (lineCount > 2) {
+      let row = decoded
+        .split(" ")
+        .filter((entry) => entry != "")
+        .map((entry) => parseInt(entry));
+      boardsInput.push(row);
+    }
+
+    lineCount++;
+  }
+
+  let board: number[][] = [];
+
+  for (const row of boardsInput) {
+    if (row.length === 0) {
+      boards.push(board);
+      board = [];
+    } else {
+      board.push(row);
+    }
+  }
+
+  return {
+    boards: boards,
+    nums: nums,
+  };
+};
