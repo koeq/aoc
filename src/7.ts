6cb455893d193d069eb749f8fc48fe40716b53fe
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-04-27 09:25:05 +0200

    7.2

--- src/7.ts
+++ src/7.ts
@@ -19,7 +19,8 @@ const createPositionTracker = (input: number[]) => {
 
 const positions = createPositionTracker(input);
 
-const getMinimalFuelCost = (positions: number[]) => {
+// 7.1
+const getMinimalConstantFuelCost = (positions: number[]) => {
   let minFuel = -1;
 
   positions.forEach((_, index) => {
@@ -40,4 +41,31 @@ const getMinimalFuelCost = (positions: number[]) => {
   return minFuel;
 };
 
-console.log(getMinimalFuelCost(positions));
+// console.log(getMinimalConstantFuelCost(positions));
+
+// 7.2
+const getMinimalLinearFuelCost = (positions: number[]) => {
+  let minFuel = -1;
+
+  positions.forEach((_, index) => {
+    let fuelCost = 0;
+
+    // calculate cost from position (index)
+    for (let i = 0; i < positions.length; i++) {
+      const dist = Math.abs(index - i);
+      // n + n-1 + ... + 1 === n*(n+1)/2
+      const singleWayCost = (dist * (dist + 1)) / 2;
+      fuelCost += singleWayCost * positions[i];
+    }
+
+    if (minFuel === -1) {
+      minFuel = fuelCost;
+    } else if (fuelCost < minFuel) {
+      minFuel = fuelCost;
+    }
+  });
+
+  return minFuel;
+};
+
+console.log(getMinimalLinearFuelCost(positions));
