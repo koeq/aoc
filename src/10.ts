b9813a6010a55512ce14618ff098fc2cae60f9e1
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-05-06 15:19:44 +0200

    10.2

--- src/10.ts
+++ src/10.ts
@@ -33,6 +33,7 @@ const closeToOpen: Brackets = {
   ">": "<",
 };
 
+// 10.1
 const getCorruptedChunks = (input: string[]): string[] => {
   const corruptedChunks: string[] = [];
   const corruptedLines: string[] = [];
@@ -82,4 +83,62 @@ const points = corruptedChunks.reduce(
   (acc, curr) => acc + pointsMapping[curr],
   0
 );
-console.log(points);
+// console.log(points);
+
+// 10.2
+const getIncompletes = (input: string[]): string[][] => {
+  const corruptedChunks: string[] = [];
+  const corruptedLines: string[] = [];
+  let notClosed: string[][] = [];
+
+  for (let line = 0; line < input.length; line++) {
+    let stack: string[] = [];
+    let isLineCorrupted = false;
+
+    // while (chunk < input[line].length && !isLineCorrupted) {
+    for (
+      let chunk = 0;
+      chunk < input[line].length && !isLineCorrupted;
+      chunk++
+    ) {
+      // chunk is opening
+      if (input[line][chunk] in openToClose) {
+        stack.push(input[line][chunk]);
+        // chunk is closing and last on the stack is matching opening
+      } else if (
+        input[line][chunk] in closeToOpen &&
+        closeToOpen[input[line][chunk]] === stack[stack.length - 1]
+      ) {
+        stack.pop();
+        // chunk corrupts line
+      } else {
+        corruptedChunks.push(input[line][chunk]);
+        corruptedLines.push(input[line]);
+        // move to next line if current line is corrupted
+        isLineCorrupted = true;
+        // clean stack on corrupted line
+        stack = [];
+      }
+    }
+    notClosed.push(stack);
+  }
+  return notClosed;
+};
+
+const notClosed = getIncompletes(input).filter((arr) => arr.length > 0);
+const mappedNotClosed = notClosed.map((arr) =>
+  arr.reverse().map((str) => openToClose[str])
+);
+
+const pointsMapping2: { [k: string]: number } = {
+  ")": 1,
+  "]": 2,
+  "}": 3,
+  ">": 4,
+};
+
+const points2 = mappedNotClosed
+  .map((arr) => arr.reduce((acc, curr) => acc * 5 + pointsMapping2[curr], 0))
+  .sort((a, b) => a - b);
+
+console.log(points2[(points2.length - 1) / 2]);
