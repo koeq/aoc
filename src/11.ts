234cbd4e5c26a0fd5e1047ee396bf74c30b25fb2
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-05-09 09:00:14 +0200

    11.1

--- src/11.ts
+++ src/11.ts
@@ -1,4 +1,19 @@
-const rawInput = ["11111", "19991", "19191", "19991", "11111"];
+import { getInput } from "./utils/file-reader";
+// const rawInput = ["11111", "19991", "19191", "19991", "11111"];
+// const rawInput = [
+//   "5483143223",
+//   "2745854711",
+//   "5264556173",
+//   "6141336146",
+//   "6357385478",
+//   "4167524645",
+//   "2176841721",
+//   "6882881134",
+//   "4846848554",
+//   "5283751526",
+// ];
+
+const rawInput = getInput(11);
 
 const input = rawInput.map((str) => str.split("").map((str) => parseInt(str)));
 
@@ -8,23 +23,26 @@ interface Point {
   value: number;
 }
 
+let counter = 0;
+
 const flash = (point: Point, input: number[][]): number[][] => {
   const { x, y, value } = point;
 
   // 3. flashed points are set to 0
   input[y][x] = 0;
-
+  counter++;
   // flash spreads to points around
   const topLeftDiagonal = y - 1 >= 0 &&
     x - 1 >= 0 && { x: x - 1, y: y - 1, value: input[y - 1][x - 1] };
 
   if (topLeftDiagonal && topLeftDiagonal.value !== 0) {
     input[y - 1][x - 1] = topLeftDiagonal.value + 1;
+    topLeftDiagonal.value = topLeftDiagonal.value + 1;
     input = topLeftDiagonal.value > 9 ? flash(topLeftDiagonal, input) : input;
   }
 
   const topRightDiagonal = y - 1 >= 0 &&
-    x + 1 < input[y].length && {
+    x + 1 < input[y - 1].length && {
       x: x + 1,
       y: y - 1,
       value: input[y - 1][x + 1],
@@ -32,6 +50,7 @@ const flash = (point: Point, input: number[][]): number[][] => {
 
   if (topRightDiagonal && topRightDiagonal.value !== 0) {
     input[y - 1][x + 1] = topRightDiagonal.value + 1;
+    topRightDiagonal.value = topRightDiagonal.value + 1;
     input = topRightDiagonal.value > 9 ? flash(topRightDiagonal, input) : input;
   }
 
@@ -40,12 +59,13 @@ const flash = (point: Point, input: number[][]): number[][] => {
 
   if (bottomLeftDiagonal && bottomLeftDiagonal.value !== 0) {
     input[y + 1][x - 1] = bottomLeftDiagonal.value + 1;
+    bottomLeftDiagonal.value = bottomLeftDiagonal.value + 1;
     input =
       bottomLeftDiagonal.value > 9 ? flash(bottomLeftDiagonal, input) : input;
   }
 
   const bottomRightDiagonal = y + 1 < input.length &&
-    x + 1 < input[y].length && {
+    x + 1 < input[y + 1].length && {
       x: x + 1,
       y: y + 1,
       value: input[y + 1][x + 1],
@@ -53,6 +73,7 @@ const flash = (point: Point, input: number[][]): number[][] => {
 
   if (bottomRightDiagonal && bottomRightDiagonal.value !== 0) {
     input[y + 1][x + 1] = bottomRightDiagonal.value + 1;
+    bottomRightDiagonal.value = bottomRightDiagonal.value + 1;
     input =
       bottomRightDiagonal.value > 9 ? flash(bottomRightDiagonal, input) : input;
   }
@@ -61,6 +82,7 @@ const flash = (point: Point, input: number[][]): number[][] => {
 
   if (top && top.value !== 0) {
     input[y - 1][x] = top.value + 1;
+    top.value = top.value + 1;
     input = top.value > 9 ? flash(top, input) : input;
   }
 
@@ -72,6 +94,7 @@ const flash = (point: Point, input: number[][]): number[][] => {
 
   if (bottom && bottom.value !== 0) {
     input[y + 1][x] = bottom.value + 1;
+    bottom.value = bottom.value + 1;
     input = bottom.value > 9 ? flash(bottom, input) : input;
   }
 
@@ -79,6 +102,7 @@ const flash = (point: Point, input: number[][]): number[][] => {
 
   if (left && left.value !== 0) {
     input[y][x - 1] = left.value + 1;
+    left.value = left.value + 1;
     input = left.value > 9 ? flash(left, input) : input;
   }
 
@@ -90,6 +114,7 @@ const flash = (point: Point, input: number[][]): number[][] => {
 
   if (right && right.value !== 0) {
     input[y][x + 1] = right.value + 1;
+    right.value = right.value + 1;
     input = right.value > 9 ? flash(right, input) : input;
   }
 
@@ -106,8 +131,8 @@ const step = (steps: number, input: number[][]) => {
     for (let y = 0; y < input.length; y++) {
       for (let x = 0; x < input[y].length; x++) {
         const point = { x, y, value: input[y][x] };
-        // 2. point value >= 10 --> flash! -> one point can only flash once
-        if (point.value >= 10) {
+        // 2. point value > 9 --> flash! -> one point can only flash once
+        if (point.value > 9) {
           input = flash(point, input);
         }
       }
@@ -117,5 +142,5 @@ const step = (steps: number, input: number[][]) => {
   return input;
 };
 
-const stepped = step(1, input);
-console.log(stepped);
+const stepped = step(100, input);
+console.log(counter);
