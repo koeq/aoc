da033ab75d00cefe310d08e3b8d88f4742d69ee2
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-05-20 10:12:22 +0200

    14.2

--- src/14.ts
+++ src/14.ts
@@ -35,6 +35,7 @@ const insertionRules = rawInput.slice(2).reduce((acc, curr) => {
   };
 }, {});
 
+// 14.1
 // algorithm
 // 1. for each step
 // 2. figure out pairs
@@ -97,7 +98,142 @@ const getMaxMinDiff = (counter: { [k: string]: number }) => {
   return max - min;
 };
 
-const template = step(10, polymerTemplate);
-const counter = count(template);
-const diff = getMaxMinDiff(counter);
-console.log(diff);
+// const template = step(3, polymerTemplate);
+// const counter = count(template);
+// const diff = getMaxMinDiff(counter);
+// console.log(diff);
+
+// 14.2
+// Template:     NNCB
+// After step 1: NCNBCHB
+// After step 2: NBCCNBBBCBHCB -> length = 13 --> pairs = length - 1 === 12
+// After step 3: NBBBCNCCNBBNBNBBCHBHHBCHB
+// After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
+
+// Pairs counted from template
+//  {
+// "NN": 1,
+// "NC": 1,
+// "CB": 1,
+// }
+// After          step 1        step2
+//  {
+// "NN": 1, -1     --> 0
+// "NC": 1, +1 -1  --> 1
+// "CB": 1, -1     --> 0
+// "CN": 0, +1     --> 1
+// "NB": 0, +1     --> 1
+// "BC": 0, +1     --> 1
+// "CH": 0, +1     --> 1
+// "HB": 0, +1     --> 1
+// }
+
+// count in the pairs:
+// => N: 3, C: 4, B: 3, H: 2
+
+// if count % 2 !== 0
+// count = Math.round(count/2) ->
+// else count = count/2
+// => N: 2, C: 2, B: 2, H: 1
+
+const createCounter = (
+  insertionRules: { [k: string]: string },
+  pairs: string[]
+) => {
+  const counter: { [k: string]: number } = {};
+
+  for (const rule in insertionRules) {
+    counter[rule] = 0;
+  }
+
+  for (const pair of pairs) {
+    counter[pair]++;
+  }
+
+  return counter;
+};
+
+const pairs = getPairs(polymerTemplate);
+const counter = createCounter(insertionRules, pairs);
+
+const count2 = (
+  counter: {
+    [k: string]: number;
+  },
+  insertionRules: {
+    [k: string]: string;
+  }
+) => {
+  // don't iterate over the object we're manipulating
+  const newCounter = { ...counter };
+
+  for (const pair in counter) {
+    if (counter[pair]) {
+      const insert = insertionRules[pair];
+      const firstPairPart = pair[0];
+      const secondPairPart = pair[1];
+      newCounter[`${firstPairPart}${insert}`] += counter[pair];
+      newCounter[`${insert}${secondPairPart}`] += counter[pair];
+      newCounter[pair] -= counter[pair];
+    }
+  }
+
+  return newCounter;
+};
+
+const step2 = (steps: number, counter: { [k: string]: number }) => {
+  for (let i = 0; i < steps; i++) {
+    counter = count2(counter, insertionRules);
+  }
+  return counter;
+};
+const counted = step2(40, counter);
+
+const countLetters = (counted: { [k: string]: number }) => {
+  const lettersCounter: { [k: string]: number } = {};
+
+  for (const pair in counted) {
+    const firstPairPart = pair[0];
+    const secondPairPart = pair[1];
+    lettersCounter[firstPairPart] =
+      (lettersCounter[firstPairPart] || 0) + counted[pair];
+    lettersCounter[secondPairPart] =
+      (lettersCounter[secondPairPart] || 0) + counted[pair];
+  }
+
+  for (const letter in lettersCounter) {
+    if (lettersCounter[letter] % 2 === 0) {
+      lettersCounter[letter] = lettersCounter[letter] / 2;
+    } else {
+      lettersCounter[letter] = Math.round(lettersCounter[letter] / 2);
+    }
+  }
+
+  return lettersCounter;
+};
+
+const lettersCounter = countLetters(counted);
+
+const getDelta = (lettersCounter: { [k: string]: number }) => {
+  let max = 0;
+  let min = 0;
+
+  for (const letter in lettersCounter) {
+    if (!max) {
+      max = lettersCounter[letter];
+    } else {
+      max = lettersCounter[letter] > max ? lettersCounter[letter] : max;
+    }
+
+    if (!min) {
+      min = lettersCounter[letter];
+    } else {
+      min = lettersCounter[letter] < min ? lettersCounter[letter] : min;
+    }
+  }
+
+  return max - min;
+};
+
+const delta = getDelta(lettersCounter);
+console.log(delta);
