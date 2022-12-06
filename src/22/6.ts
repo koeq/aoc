355e67595b7379b3b37232012d89c4a759b1940a
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-12-06 11:27:20 +0100

    6-2

--- src/22/6.ts
+++ src/22/6.ts
@@ -2,7 +2,7 @@ import { getInput } from "../../get-input";
 
 const input = getInput("./src/22/inputs/input-6.txt");
 
-const getProcessedCountBeforeFourUnique = () => {
+const getProcessedCountBeforeFourUnique = (processStep: number) => {
   if (!input) {
     return;
   }
@@ -21,12 +21,16 @@ const getProcessedCountBeforeFourUnique = () => {
   };
 
   // 0 1 2 3 4
-  for (let i = 0; i < input.length - 3; i++) {
-    if (getMaxCharCount(input.slice(i, i + 4).split("")) === 1) {
-      return i + 4;
+  for (let i = 0; i <= input.length - processStep; i++) {
+    if (getMaxCharCount(input.slice(i, i + processStep).split("")) === 1) {
+      return i + processStep;
     }
   }
 };
 
-const processedCount = getProcessedCountBeforeFourUnique();
+// 1
+// const processedCount = getProcessedCountBeforeFourUnique(4);
+
+// 2
+const processedCount = getProcessedCountBeforeFourUnique(14);
 console.log(processedCount);
