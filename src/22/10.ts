3d3e40cce6e85218feede8c45d57b2273700a0b4
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-12-10 10:52:40 +0100

    10-1

---
+++ src/22/10.ts
@@ -0,0 +1,193 @@
+import { getInput } from "../../get-input";
+
+// const testInput = `addx 15
+// addx -11
+// addx 6
+// addx -3
+// addx 5
+// addx -1
+// addx -8
+// addx 13
+// addx 4
+// noop
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx 5
+// addx -1
+// addx -35
+// addx 1
+// addx 24
+// addx -19
+// addx 1
+// addx 16
+// addx -11
+// noop
+// noop
+// addx 21
+// addx -15
+// noop
+// noop
+// addx -3
+// addx 9
+// addx 1
+// addx -3
+// addx 8
+// addx 1
+// addx 5
+// noop
+// noop
+// noop
+// noop
+// noop
+// addx -36
+// noop
+// addx 1
+// addx 7
+// noop
+// noop
+// noop
+// addx 2
+// addx 6
+// noop
+// noop
+// noop
+// noop
+// noop
+// addx 1
+// noop
+// noop
+// addx 7
+// addx 1
+// noop
+// addx -13
+// addx 13
+// addx 7
+// noop
+// addx 1
+// addx -33
+// noop
+// noop
+// noop
+// addx 2
+// noop
+// noop
+// noop
+// addx 8
+// noop
+// addx -1
+// addx 2
+// addx 1
+// noop
+// addx 17
+// addx -9
+// addx 1
+// addx 1
+// addx -3
+// addx 11
+// noop
+// noop
+// addx 1
+// noop
+// addx 1
+// noop
+// noop
+// addx -13
+// addx -19
+// addx 1
+// addx 3
+// addx 26
+// addx -30
+// addx 12
+// addx -1
+// addx 3
+// addx 1
+// noop
+// noop
+// noop
+// addx -9
+// addx 18
+// addx 1
+// addx 2
+// noop
+// noop
+// addx 9
+// noop
+// noop
+// noop
+// addx -1
+// addx 2
+// addx -37
+// addx 1
+// addx 3
+// noop
+// addx 15
+// addx -21
+// addx 22
+// addx -6
+// addx 1
+// noop
+// addx 2
+// addx 1
+// noop
+// addx -10
+// noop
+// noop
+// addx 20
+// addx 1
+// addx 2
+// addx 2
+// addx -6
+// addx -11
+// noop
+// noop
+// noop`;
+
+// const instructions = testInput.split("\n");
+
+const instructions = getInput("./src/22/inputs/input-10.txt")?.split("\n")!;
+instructions.pop();
+
+const getSignalStrengthSum = () => {
+  const cycles = [20, 60, 100, 140, 180, 220];
+  let cycle = 0;
+  let X = 1;
+  let sum = 0;
+
+  for (const input of instructions) {
+    const [instruction, amount] = input.split(" ");
+
+    if (instruction === "noop") {
+      cycle++;
+      if (cycles.includes(cycle)) {
+        sum += cycle * X;
+      }
+      continue;
+    }
+
+    // read
+    cycle++;
+
+    if (cycles.includes(cycle)) {
+      sum += cycle * X;
+    }
+    // execute
+    cycle++;
+
+    if (cycles.includes(cycle)) {
+      sum += cycle * X;
+    }
+
+    // increment
+    X = X + parseInt(amount);
+  }
+
+  return sum;
+};
+
+const sum = getSignalStrengthSum();
+console.log(sum);
