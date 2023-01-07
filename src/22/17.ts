5f7aa9de97de5293436a48f1f59e18f983a65052
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2023-01-07 18:47:04 +0100

    17 create helpers

--- src/22/17.ts
+++ src/22/17.ts
@@ -1,13 +1,14 @@
-import { isIfStatement } from "typescript";
 import { getInput } from "./get-input";
 
-// const input = getInput("./src/22/inputs/input-17.txt")!.split("\n");
-// input.pop();
+// const jets = getInput("./src/22/inputs/input-17.txt")!.split("\n");
+// jets.pop();
 
-const printArrAsString = (content: unknown[]) => {
-  for (const row of content) {
+const printArrAsString = (arr: unknown[]) => {
+  for (const row of arr) {
     console.log(JSON.stringify(row));
   }
+
+  console.log("\n");
 };
 
 const jets = `>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>`;
@@ -78,9 +79,10 @@ const extendTower = (tower: Content[][], shapeNum: number) => {
   tower.unshift([".", ".", ".", ".", ".", ".", "."]);
   const shape = shapes.get(shapeNum)!;
 
-  for (const row of shape) {
-    tower.unshift(row);
+  for (let row = shape.length - 1; row >= 0; row--) {
+    tower.unshift(shape[row]);
   }
+  return tower;
 };
 
 const blow = (tower: Content[][], jet: string) => {
@@ -118,39 +120,74 @@ const blow = (tower: Content[][], jet: string) => {
   return towerCopy;
 };
 
-const moveShape = (tower: Content[][]) => {};
+const moveShape = (tower: Content[][]) => {
+  const towerCopy = tower.map((arr) => arr.slice());
 
-const shapeToRock = (tower: Content[][]) => {};
+  for (let row = tower.length - 1; row >= 0; row--) {
+    for (let col = tower[row].length - 1; col >= 0; col--) {
+      if (tower[row][col] === "@") {
+        towerCopy[row + 1][col] = "@";
+        towerCopy[row][col] = ".";
+      }
+    }
+  }
 
-const trimTower = (tower: Content[][]) => {};
+  return towerCopy;
+};
+
+const shapeToRock = (tower: Content[][]) => {
+  tower.forEach((row) => {
+    for (let i = 0; i < row.length; i++) {
+      if (row[i] === "@") row[i] = "#";
+    }
+  });
+
+  return tower;
+};
+
+const trimTower = (tower: Content[][]): Content[][] => {
+  let lastRockIndex: number = Infinity;
+
+  for (let row = 0; row < tower.length; row++) {
+    for (const content of tower[row]) {
+      if (content === "#") {
+        lastRockIndex = row;
+        return tower.slice(lastRockIndex);
+      }
+    }
+  }
+
+  return [];
+};
 
 function simulate(rocksNum: number) {
   let tower: Content[][] = [];
-
   let shapeNum = 0;
-  let jet: string;
   let jetIndex = 0;
-  const startingCol = 2;
-  let lastRockMaxX = 0;
 
   for (let i = 0; i < rocksNum; i++) {
     shapeNum = getShapeNum(shapeNum);
-    jet = jets[jetIndex % jets.length];
 
     // add rows to beginning of tower so that last rock and new rock are three rows apart
     extendTower(tower, shapeNum);
 
-    printArrAsString(tower);
-
+    // this logic is not correct
     while (!sittingOnRockOrBottom(tower)) {
-      tower = blow(tower, jet);
+      tower = blow(tower, jets[jetIndex % jets.length]);
       jetIndex++;
-      moveShape(tower);
+      tower = moveShape(tower);
     }
 
-    shapeToRock(tower);
-    trimTower(tower);
+    tower = blow(tower, jets[jetIndex % jets.length]);
+    jetIndex++;
+
+    tower = shapeToRock(tower);
+    tower = trimTower(tower);
   }
+
+  printArrAsString(tower);
+
+  return tower;
 }
 
-simulate(1);
+console.log(simulate(3).length);
