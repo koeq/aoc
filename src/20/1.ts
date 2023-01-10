daeb06d534506e2d4ee49f0dad0800892c06d209
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2023-01-10 10:24:37 +0100

    2020 1-2

--- src/20/1.ts
+++ src/20/1.ts
@@ -8,7 +8,7 @@ input.pop();
 const sum = 2020;
 
 // C(n,r) = n!/(r!(n-r)!) -> C are the combinations, n the objects, r the samples
-function sumEquals(input: number[]) {
+function twoSumEquals(input: number[]) {
   for (let i = 0; i < input.length - 1; i++) {
     for (let j = i + 1; j < input.length; j++) {
       if (input[i] + input[j] === sum) {
@@ -20,5 +20,22 @@ function sumEquals(input: number[]) {
   return [];
 }
 
-const [first, second] = sumEquals(input);
-console.log(first * second);
+// const [first, second] = twoSumEquals(input);
+// console.log(first * second);
+
+function threeSumEquals(input: number[]) {
+  for (let i = 0; i < input.length - 2; i++) {
+    for (let j = i + 1; j < input.length - 1; j++) {
+      for (let z = j + 1; z < input.length; z++) {
+        if (input[i] + input[j] + input[z] === sum) {
+          return [input[i], input[j], input[z]];
+        }
+      }
+    }
+  }
+
+  return [];
+}
+
+const [first, second, third] = threeSumEquals(input);
+console.log(first * second * third);
