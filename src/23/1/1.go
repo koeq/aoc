187f7e12d8a42c87a216b3e77b85a937e4ef56d2
koeq <koe.be@web.de>
2023-12-02 11:59:55 +0100

    23 1-2

--- src/23/1/1.go
+++ src/23/1/1.go
@@ -6,9 +6,104 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
 	"strconv"
+	"strings"
 	"unicode"
 )
+
+var numbers = map[string]string{
+	"zero":  "0",
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+}
+
+type Match struct {
+	Word  string
+	Index int
+}
+
+func findNumberWords(input string) []string {
+	numberWords := []string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	var matches []Match
+
+	for _, word := range numberWords {
+		start := 0
+		for {
+			index := strings.Index(input[start:], word)
+			if index == -1 {
+				break
+			}
+			matches = append(matches, Match{Word: word, Index: start + index})
+			start += index + 1 // Move past the last found index
+		}
+	}
+
+	// Sort matches based on their indices
+	sort.Slice(matches, func(i, j int) bool {
+		return matches[i].Index < matches[j].Index
+	})
+
+	// Extract just the words from the sorted matches
+	var orderedMatches []string
+	for _, match := range matches {
+		orderedMatches = append(orderedMatches, match.Word)
+	}
+
+	return orderedMatches
+}
+
+func getNumString(line string) string {
+	splittedLine := make([]string, 10)
+	numStr := ""
+	charBuff := ""
+
+	for _, char := range line {
+		// case NUMBER
+		if unicode.IsDigit(char) {
+			if charBuff != "" {
+				// add prev charBuff
+				splittedLine = append(splittedLine, charBuff)
+			}
+
+			// reset buffer
+			charBuff = ""
+
+			splittedLine = append(splittedLine, string(char))
+			continue
+		}
+
+		// case CHAR
+		charBuff += string(char)
+
+	}
+
+	if charBuff != "" {
+		splittedLine = append(splittedLine, charBuff)
+	}
+
+	for _, str := range splittedLine {
+		_, err := strconv.Atoi(str)
+
+		if err == nil {
+			numStr += str
+		}
+
+		numberWords := findNumberWords(str)
+		for _, number := range numberWords {
+			numStr += numbers[number]
+		}
+	}
+
+	return numStr
+}
 
 func firstAndLast(s string) string {
 	if len(s) == 1 {
@@ -20,18 +115,6 @@ func firstAndLast(s string) string {
 	}
 
 	return s
-}
-
-func getNumString(line string) string {
-	numStr := ""
-
-	for _, char := range line {
-		if unicode.IsDigit(char) {
-			numStr += string(char)
-		}
-	}
-
-	return numStr
 }
 
 func main() {
