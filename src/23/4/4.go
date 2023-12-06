494384a2f9cf095a8045c8ab2c7da868662ba47f
koeq <koe.be@web.de>
2023-12-06 10:44:51 +0100

    4.2

--- src/23/4/4.go
+++ src/23/4/4.go
@@ -5,15 +5,11 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 )
 
-func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		panic(err)
-	}
-
+func one(file *os.File) {
 	getLine := utils.CreateGetLine(file)
 	sum := 0
 
@@ -51,4 +47,101 @@ func main() {
 	}
 
 	fmt.Println(sum)
+}
+
+type card struct {
+	instances   int
+	winningNums []int
+	nums        []int
+}
+
+func createCard(line []rune) *card {
+	card := card{instances: 1, winningNums: make([]int, 0, 50), nums: make([]int, 0, 50)}
+
+	joinedNums := strings.Split(strings.Split(string(line), ":")[1], "|")
+	winningNums := strings.Fields(joinedNums[0])
+	nums := strings.Fields(joinedNums[1])
+
+	for _, winning := range winningNums {
+		w, err := strconv.Atoi(winning)
+		if err != nil {
+			panic(err)
+		}
+
+		card.winningNums = append(card.winningNums, w)
+	}
+
+	for _, num := range nums {
+		n, err := strconv.Atoi(num)
+		if err != nil {
+			panic(err)
+		}
+
+		card.nums = append(card.nums, n)
+	}
+
+	return &card
+}
+
+func getMatchCount(card card) int {
+	counter := 0
+
+	for _, w := range card.winningNums {
+		for _, n := range card.nums {
+			if w != n {
+				continue
+			}
+
+			counter++
+		}
+	}
+
+	return counter
+}
+
+func two(file *os.File) {
+	lines, err := utils.GetLines(file)
+	if err != nil {
+		panic(err)
+	}
+
+	var cards []card
+
+	for _, line := range lines {
+		card := *createCard(line)
+		cards = append(cards, card)
+	}
+
+	for currIndex, card := range cards {
+		matchCount := getMatchCount(card)
+
+		if matchCount == 0 {
+			continue
+		}
+
+		// Add one instance for each instance of our current card as long as we have match counts and cards left
+		for i := 1; i <= matchCount && currIndex+i < len(cards); i++ {
+			cards[currIndex+i].instances += 1 * card.instances
+		}
+	}
+
+	sum := 0
+	for _, card := range cards {
+		sum += card.instances
+	}
+
+	fmt.Println(sum)
+}
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	// one(file)
+
+	two((file))
+
+	file.Close()
 }
